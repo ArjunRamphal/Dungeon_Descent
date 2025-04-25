b3df439fb8bdb8cf3a3eb20d3bf834d1027a1974
ZaidKajee <zaidkajee2004@gmail.com>
2025-04-25 22:55:51 +0200

    Update Battle.h

--- x64/Debug/Battle.h
+++ x64/Debug/Battle.h
@@ -7,28 +7,39 @@
 #include "RoomBase.h"
 #include "Character.h"
 #include "test.h"  
+#include "CriticalStrike.h"
 
 class Battle : public RoomBase {
 private:
     test* enemy;  // Declare a pointer to the test class (the enemy)
+    Character* character;
     int strikesRemaining;  // Number of strikes remaining for the player
     int extra;  // Extra strikes based on player agility
+    bool isBoss;
+
+
+    CriticalStrike<int> intCrit{ 2 };       // 2x multiplier for integers
+    CriticalStrike<float> floatCrit{ 1.5f }; // 1.5x multiplier for floats
 public:
     //constructor
     Battle(bool isBoss, Character& player);  // Constructor for Battle
     //methods
     string getImageFileName(int biome);
     string getTextFileName(int biome);
-    void attack(Character& player);  // Method to handle player attack
+    bool attack(Character& player);  // Method to handle player attack
     int Calculate_Extra_Strikes(int agility);  // Calculate extra strikes based on agility
     bool isCrit_Strike(int accuracy);  // Determine if it's a critical strike
     int Total_Strikes();  // Total number of strikes (including extra strikes)
     bool isBattleFinished();  // Check if the battle is finished (based on strikes or defeat)
-    int getStrikesRemaining() const { return strikesRemaining; }  // Getter for strikes remaining
+    int getStrikesRemaining() const { 
+        return strikesRemaining; 
+    }  // Getter for strikes remaining
     // Getter for enemy (accessor method)
     test* getEnemy() const {
         return enemy;
     }
+    vector<string> getChoices(int roomCounter, int biome) override;
+    string getbtnChoice2(Character& character) override;
 
     //destructor
     ~Battle() {
