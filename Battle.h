eed701f412df657f9e6f809e15b923de46ad75dd
ZaidKajee <zaidkajee2004@gmail.com>
2025-04-25 22:50:31 +0200

    Update Battle.h with Critical Strike Class Template

--- Battle.h
+++ Battle.h
@@ -6,34 +6,40 @@
 #include <string>
 #include "RoomBase.h"
 #include "Character.h"
-#include "Monster.h"  
+#include "test.h"  
+#include "CriticalStrike.h"
 
 class Battle : public RoomBase {
 private:
-    Monster* enemy;  // Declare a pointer to the Monster class (the enemy)
+    test* enemy;  // Declare a pointer to the test class (the enemy)
     Character* character;
     int strikesRemaining;  // Number of strikes remaining for the player
     int extra;  // Extra strikes based on player agility
     bool isBoss;
+
+
+    CriticalStrike<int> intCrit{ 2 };       // 2x multiplier for integers
+    CriticalStrike<float> floatCrit{ 1.5f }; // 1.5x multiplier for floats
 public:
     //constructor
     Battle(bool isBoss, Character& player);  // Constructor for Battle
     //methods
     string getImageFileName(int biome);
     string getTextFileName(int biome);
-    string getbtnChoice2(Character& character) override;
-
-    int Calculate_Extra_Strikes(int agility);  // Calculate extra strikes based on agility
-    int Total_Strikes();  // Total number of strikes (including extra strikes)
-    int getStrikesRemaining() const;  // Getter for strikes remaining
-
     bool attack(Character& player);  // Method to handle player attack
+    int Calculate_Extra_Strikes(int agility);  // Calculate extra strikes based on agility
     bool isCrit_Strike(int accuracy);  // Determine if it's a critical strike
+    int Total_Strikes();  // Total number of strikes (including extra strikes)
     bool isBattleFinished();  // Check if the battle is finished (based on strikes or defeat)
-
+    int getStrikesRemaining() const { 
+        return strikesRemaining; 
+    }  // Getter for strikes remaining
+    // Getter for enemy (accessor method)
+    test* getEnemy() const {
+        return enemy;
+    }
     vector<string> getChoices(int roomCounter, int biome) override;
-
-    Monster* getEnemy() const;// Getter for enemy (accessor method)
+    string getbtnChoice2(Character& character) override;
 
     //destructor
     ~Battle() {
